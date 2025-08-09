b4ba3977fcbc3ff59a5a9de814004fac0a0750d8
Rob Archibald <robarchibald@myprice.health>
2025-08-08 18:21:45 -0700

    FinalPricing in PricedClaim

---
+++ mph/error.go
@@ -0,0 +1,250 @@
+package web
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"braces.dev/errtrace"
+	"github.com/mypricehealth/mono/pkg/client/notify"
+	"github.com/mypricehealth/mono/pkg/test"
+	"github.com/mypricehealth/mono/pkg/traceutils"
+	"github.com/stretchr/testify/assert"
+)
+
+type Error struct {
+	Title     string
+	Detail    error
+	ErrorCode int // will be put into Response StatusCode
+}
+
+func (e *Error) IsFatal() bool {
+	return e != nil && e.ErrorCode >= 500 && e.ErrorCode <= 599
+}
+
+type errorJSON struct {
+	Title     string `json:"title,omitempty"`
+	ErrorCode int    `json:"errorCode,omitempty"`
+}
+
+func ErrorEqual(t *testing.T, want *Error, got *Error) {
+	var wantCopy *Error
+	if want != nil {
+		wantCopy = new(Error)
+		*wantCopy = *want
+
+		wantCopy.Detail = test.UnwrapErrtrace(wantCopy.Detail)
+	}
+
+	var gotCopy *Error
+	if got != nil {
+		gotCopy = new(Error)
+		*gotCopy = *got
+
+		gotCopy.Detail = test.UnwrapErrtrace(gotCopy.Detail)
+	}
+
+	assert.Equal(t, wantCopy, gotCopy)
+}
+
+func (e *Error) Unwrap() error {
+	if e == nil {
+		return nil
+	}
+
+	return e.Detail
+}
+
+func (e *Error) Error() string {
+	if e == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s: %s", e.Title, e.Detail)
+}
+
+func (e *Error) MarshalJSON() ([]byte, error) {
+	if e == nil || e.Detail == nil {
+		return errtrace.Wrap2(json.Marshal(nil))
+	}
+
+	if !e.IsFatal() {
+		panic(errtrace.Errorf("cannot marshal non-fatal errors to JSON, use ResponseError instead"))
+	}
+
+	return errtrace.Wrap2(json.Marshal(errorJSON{
+		Title:     e.Title,
+		ErrorCode: e.ErrorCode,
+	}))
+}
+
+func (e *Error) UnmarshalJSON(data []byte) error {
+	var ej errorJSON
+	if err := json.Unmarshal(data, &ej); err != nil {
+		return errtrace.Wrap(err)
+	}
+	e.Title = ej.Title
+	e.ErrorCode = ej.ErrorCode
+	return nil
+}
+
+var _ json.Marshaler = &Error{}
+var _ json.Unmarshaler = &Error{}
+
+func (e *Error) Is(err error) bool {
+	return (e == nil && err == nil) || e != nil && e.Detail != nil && errors.Is(e.Detail, err)
+}
+
+func (e *Error) ToResponseError() *ResponseError {
+	if e == nil || e.Detail == nil || (e.Title == "" && e.Detail.Error() == "") {
+		return nil
+	}
+	if e.IsFatal() {
+		panic(errtrace.Errorf("fatal web.Errors cannot be converted to ResponseError"))
+	}
+	return &ResponseError{
+		Title:  e.Title,
+		Detail: e.Detail.Error(),
+	}
+}
+
+type ResponseError struct {
+	Title  string `json:"title,omitempty"  db:"-"`
+	Detail string `json:"detail,omitempty" db:"error_detail"`
+}
+
+var _ error = &ResponseError{}
+
+func NewResponseError(title string, detail error) *ResponseError {
+	if detail == nil || detail.Error() == "" {
+		return nil
+	}
+	return NewResponseErrorText(title, detail.Error())
+}
+
+func NewResponseErrorText(title, detail string) *ResponseError {
+	return &ResponseError{
+		Title:  title,
+		Detail: detail,
+	}
+}
+
+func (r *ResponseError) Error() string {
+	if r == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s: %s", r.Title, r.Detail)
+}
+
+func (r *ResponseError) ToClientError() *Error {
+	if r == nil {
+		return nil
+	}
+	return ClientError(r.Title, errtrace.Errorf("%s", r.Detail))
+}
+
+func NewError(title string, err error, errorCode int) *Error {
+	if err == nil {
+		return nil
+	}
+	return &Error{
+		Title:     title,
+		Detail:    err,
+		ErrorCode: errorCode,
+	}
+}
+
+func ForbiddenError(detail error) *Error {
+	return NewError("Forbidden", detail, http.StatusForbidden)
+}
+
+func ClientError(title string, detail error) *Error {
+	var wErr *Error
+	if errors.As(detail, &wErr) {
+		if wErr.IsFatal() {
+			panic(errtrace.Errorf("cannot create client error from fatal error"))
+		}
+	}
+	return NewError(title, detail, http.StatusBadRequest)
+}
+
+func InternalError(title string, detail error) *Error {
+	return NewError(title, detail, http.StatusInternalServerError)
+}
+
+// HandleInternalError is a convenience function that.
+func HandleInternalError(w http.ResponseWriter, notifier notify.Client, errDetail error, title string) bool {
+	return HandleError(w, notifier, InternalError(title, errDetail))
+}
+
+func HandleClientError(w http.ResponseWriter, errDetail error, title string) bool {
+	return HandleError(w, nil, ClientError(title, errDetail))
+}
+
+// HandleError checks the err provided to see if it is not nil. If the error is not nil, HandleError will:
+// 1. Output JSON to the client with the title provided in the `title` parameter for the error title.
+// 2. Notify support provided that `IsFatal` is true and `client` is not nil.
+// 3. Returns true if there is an error. False if there is no error.
+func HandleError(w http.ResponseWriter, notifier notify.Client, err *Error) bool {
+	if err != nil && err.Detail != nil {
+		WriteError(w, notifier, err.Title, err.Detail.Error(), err.ErrorCode)
+
+		if err.IsFatal() {
+			fmt.Fprintf(os.Stderr, "Web API error: %s: %s\n", err.Title, err.Detail)
+			if notifier != nil {
+				go notifier.SendMessage(fmt.Sprintf("Web API error: %s: %s\n", err.Title, err.Detail)) //nolint:errcheck // error is checked and logged in notifier
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
+
+// WriteError writes an error to the http.ResponseWriter using a standard format like:
+//
+//	{
+//		"error": "error message",
+//		"status": 400
+//	}
+func WriteError(w http.ResponseWriter, client notify.Client, title, detail string, httpStatusCode int) {
+	writeErrorJSON(w, client, Response[any]{
+		Error: &ResponseError{
+			Title:  title,
+			Detail: detail,
+		},
+		StatusCode: httpStatusCode,
+	}, httpStatusCode)
+}
+
+func EndSpan(span traceutils.Span, webErrPtr **Error) {
+	var err error
+	if webErrPtr != nil {
+		webErr := *webErrPtr
+		if webErr != nil {
+			err = webErr
+		}
+	}
+
+	span.EndErr(&err)
+}
+
+// Call UnwrapError to unwrap `err` into a `*web.Error`.
+// If `err` is not a `*web.Error` it will be turned into an internal error with the given title.
+func UnwrapError(err error, internalErrorTitle string) *Error {
+	err = test.UnwrapErrtrace(err)
+
+	//nolint:errorlint // Avoids using `errors.As` because a web error wrapped in another error could indicate something completely different.
+	if webErr, ok := err.(*Error); ok {
+		if webErr == nil {
+			return InternalError(internalErrorTitle, errtrace.Errorf("a nil *web.Error was cast to an error, making the error non-nil"))
+		}
+
+		return webErr
+	}
+
+	return InternalError(internalErrorTitle, err)
+}
